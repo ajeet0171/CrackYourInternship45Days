5a9ec85d38e070399c6165c2fdd1bb2f9ee53fd3
Ajeet Kumar <102899485+ajeet0171@users.noreply.github.com>
2022-05-20 19:21:46 +0530

    Update Sort Colors.py

--- Day1/Sort Colors.py
+++ Day1/Sort Colors.py
@@ -1,3 +1,4 @@
+# dutch national flag algorithm
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         lo,mid,hi=0,0,len(nums)-1
@@ -12,3 +13,17 @@ class Solution:
                 hi-=1
             else:
                 mid+=1
+# method 2 using count sort
+# count sort    
+def sortColors1(self, nums):
+    c0 = c1 = c2 = 0
+    for num in nums:
+        if num == 0:
+            c0 += 1
+        elif num == 1:
+            c1 += 1
+        else:
+            c2 += 1
+    nums[:c0] = [0] * c0
+    nums[c0:c0+c1] = [1] * c1
+    nums[c0+c1:] = [2] * c2
